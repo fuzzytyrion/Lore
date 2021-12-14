22ee0c922eba71565d7f003b8628391e9820752c
Matt Dennis <matthewrdennis@gmail.com>
2021-12-14 20:29:33 +0000

    Add swagger

--- Lore.Api/Program.cs
+++ Lore.Api/Program.cs
@@ -1,5 +1,24 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Lore API", Description = "Adding some knowledge to my brain", Version = "v1" });
+});
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Lore API V1");   
+});    
 
 app.MapGet("/", () => "Hello World!");
 
